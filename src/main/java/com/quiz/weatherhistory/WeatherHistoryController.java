3a52a0d536fd00463a2353b48cd8bee78f7091d5
test12 <test12@test.com>
2025-05-07 21:46:02 +0900

    #temporals.format 추가 적용

--- src/main/java/com/quiz/weatherhistory/WeatherHistoryController.java
+++ src/main/java/com/quiz/weatherhistory/WeatherHistoryController.java
@@ -1,5 +1,28 @@
 package com.quiz.weatherhistory;
 
-public class WeatherHistoryController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.quiz.weatherhistory.domain.WeatherHistory;
 
+@RequestMapping("/weatherHistory")
+@Controller
+public class WeatherHistoryController {
+	//Lesson04Quiz02Controller 참고하기
+	@GetMapping("/add-weather-view")
+	public String addWeatherHistoryView() {
+		//http://localhost:8080/weatherHistory/add-weather-view
+		//날씨등록화면
+		return "weatherHistory/addWeather";
+	}
+	@PostMapping("/weather-list-view")
+	public String addWeatherHistory(@ModelAttribute WeatherHistory weatherHistory,Model model) {
+		//날씨등록 DB insert
+		//날씨등록 자료 가져오기
+		//날씨등록 후 리스트 보여주기 
+		return "weatherHistory/weatherList";
+	}
 }
---
+++ src/main/java/com/quiz/weatherhistory/domain/WeatherHistory.java
@@ -0,0 +1,69 @@
+package com.quiz.weatherhistory.domain;
+
+import java.time.LocalDateTime;
+
+public class WeatherHistory {
+	public int id;
+	public String date;
+	public String weather;
+	public Double temperatures;
+	public Double precipitation;
+	public String microDust;
+	public Double windSpeed;
+	public LocalDateTime createdAt;
+	public LocalDateTime  updatedAt;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getWeather() {
+		return weather;
+	}
+	public void setWeather(String weather) {
+		this.weather = weather;
+	}
+	public Double getTemperatures() {
+		return temperatures;
+	}
+	public void setTemperatures(Double temperatures) {
+		this.temperatures = temperatures;
+	}
+	public Double getPrecipitation() {
+		return precipitation;
+	}
+	public void setPrecipitation(Double precipitation) {
+		this.precipitation = precipitation;
+	}
+	public String getMicroDust() {
+		return microDust;
+	}
+	public void setMicroDust(String microDust) {
+		this.microDust = microDust;
+	}
+	public Double getWindSpeed() {
+		return windSpeed;
+	}
+	public void setWindSpeed(Double windSpeed) {
+		this.windSpeed = windSpeed;
+	}
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}	
+}
